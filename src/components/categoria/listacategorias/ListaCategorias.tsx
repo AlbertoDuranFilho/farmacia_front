6f1b13ee51cf1a7018918241bda0cb63a8a99333
Alberto Duran <albertoduranfilho@gmail.com>
2026-02-25 15:20:42 -0300

    :sparkles: feat: Adicionando Lista de categorias.

---
+++ src/components/categoria/listacategorias/ListaCategorias.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { SyncLoader } from "react-spinners";
+import type Categoria from "../../../models/Categoria";
+import { buscar } from "../../../services/Service";
+import { ToastAlerta } from "../../../utils/ToastAlerta";
+import CardCategoria from "../cardcategoria/CardCategoria";
+
+function ListaCategorias() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
+
+  useEffect(() => {
+    buscarCategoria();
+  }, [categorias.length]);
+
+  async function buscarCategoria() {
+    try {
+      setIsLoading(true);
+
+      await buscar("/categorias", setCategorias);
+    } catch (error: any) {
+      if (error.toString().includes("401")) {
+        ToastAlerta("Nenhuma Categoria foi encontrado!", "erro");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <>
+      {isLoading && <SyncLoader color="#312e81" size={32} />}
+      <div className="flex justify-center w-full my-4">
+        <div className="container flex flex-col">
+          {!isLoading && categorias.length === 0 && (
+            <span className="text-3xl text-center my-8">
+              Nenhuma Categoria foi encontrado!
+            </span>
+          )}
+          <div
+            className="grid grid-cols-1 md:grid-cols-2 
+                                    lg:grid-cols-3 gap-8"
+          >
+            {categorias.map((categoria) => (
+              <CardCategoria key={categoria.id} categoria={categoria} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default ListaCategorias;

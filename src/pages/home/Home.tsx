ba30306b4b8d065c8427b8000940fff36890235b
Alberto Duran <albertoduranfilho@gmail.com>
2026-02-25 14:37:46 -0300

    :lipstick: feat: Alterando estilização e palavras.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,10 +6,8 @@ function Home() {
       <div className="bg-purple-900 flex justify-center">
         <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className="text-5xl font-bold">Farmácia</h2>
-            <p className="text-xl">
-              Se até seu deploy precisa de saúde, imagina você.
-            </p>
+            <h2 className="text-5xl font-bold">PharmaClick</h2>
+            <p className="text-xl">A saúde a um clique de você.</p>
 
             <div className="flex justify-around gap-4"></div>
           </div>
